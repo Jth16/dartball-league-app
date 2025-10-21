d5fadaf82f2c62a7e2def0627c7eaf9ed2ddfef0
Jth16 <jhaluska@pcnclosings.com>
2025-10-20 22:48:34 -0400

    Updates

--- newfrontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders header image', () => {
-  render(<App />);
-  const headerImage = screen.getByAltText(/header image/i);
-  expect(headerImage).toBeInTheDocument();
-});
